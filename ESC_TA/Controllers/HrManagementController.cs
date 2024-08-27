998c7a0f626d6b4c21b3ee69cfb5e2a95d4d9b91
Angela Renae Hoch <hoch.angela.r@gmail.com>
2024-08-27 12:22:08 -0400

    file creation, api setup

---
+++ ESC_TA/Controllers/HrManagementController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESC_TA.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HrManagementController : ControllerBase
+{
+    public HrManagementController()
+    {
+
+    }
+
+    [HttpGet("employees")]
+    public IActionResult GetEmployeeData()
+    {
+        try
+        {
+            return Ok(null);
+        }
+        catch (Exception exception)
+        { 
+            return BadRequest(exception.Message);
+        }
+    }
+
+    /// <summary>
+    /// Verb used in restful name dileberately to meet requirements. Suggest refactor to return all employees if no query params given and combine with GetEmployeeData function
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="firstName"></param>
+    /// <param name="lastName"></param>
+    /// <param name="email"></param>
+    /// <param name="departmentName"></param>
+    /// <param name="country"></param>
+    /// <param name="regionName"></param>
+    /// <returns></returns>
+    [HttpGet("employees/search")]
+    public IActionResult EmployeeSearch(int? id, string? firstName, string? lastName, string? email, string? departmentName, string? country, string? regionName)
+    {
+        try
+        {
+            return Ok(null);
+        }
+        catch (Exception exception)
+        {
+            return BadRequest(exception.Message);
+        }
+    }
+}
